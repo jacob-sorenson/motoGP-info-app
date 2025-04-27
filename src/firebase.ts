dd32e8d3c07963c41527fcf45b7aa6028c843e7b
Jacob Sorenson <jacob.p.sorenson@gmail.com>
2025-04-27 00:27:26 -0600

    Drawer publish

---
+++ src/firebase.ts
@@ -0,0 +1,24 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getAuth } from "firebase/auth";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCrhqbtwjZQBTg-dIMQDv_z1Q1GmS80duI",
+//   authDomain: "motogp-info-app-backend.firebaseapp.com",
+//   projectId: "motogp-info-app-backend",
+//   storageBucket: "motogp-info-app-backend.firebasestorage.app",
+//   messagingSenderId: "810321778360",
+//   appId: "1:810321778360:web:687800b4f801f9d282c64d",
+//   measurementId: "G-VR3B72GKYC"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const auth = getAuth(app);
